2367a97be8b3aa6efa9ac4907f0e22bef6a4e886
Shikhar2001Bhadani <shkrbh30052001@gmail.com>
2025-02-05 14:13:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function makeid(l) {
   // write your code here
+	let res="";
+	let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+	for(let i=0;i<l;i++){
+		res+= chars.charAt(Math.floor(Math.random()*chars.length));
+	}
+	return res;
 }
 
 // Do not change the code below.
+
 const l = prompt("Enter a number.");
 alert(makeid(l));
